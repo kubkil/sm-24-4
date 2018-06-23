d96731f9e76ea12193928255649d3fcb95da90ba
Jakub Kilar <kuba.kilar@gmail.com>
2018-06-23 20:52:36 +0200

    finish formatDate function

--- index.js
+++ index.js
@@ -4,19 +4,23 @@ const formatDate = timeInSeconds => {
   const seconds = Math.floor(timeInSeconds % 3600 % 60);
 
   let time;
-  let showHours;
-  let showMinutes;
-
-  if(hours > 0) {
-    showHours = `${hours}h`;
-  }
-
-  if (minutes > 0) {
-    showMinutes = `${minutes}h`;
-  }
 
   if (hours < 1 && minutes < 1) {
     time = `${seconds}s`;
+  } else if (timeInSeconds == undefined) {
+    time = `0s`;
+  } else if (hours < 1 && (seconds % 60 !== 0)) {
+    time = `${minutes}m ${seconds}s`;
+  } else if (hours < 1 && (seconds % 60 === 0)) {
+    time = `${minutes}m`;
+  } else if (hours > 0 && (minutes % 60 !== 0) && (seconds % 60 !== 0)) {
+    time = `${hours}h ${minutes}m ${seconds}s`;
+  } else if (hours > 0 && (minutes % 60 === 0) && (seconds % 60 === 0)) {
+    time = `${hours}h`;
+  } else if (hours > 0 && (minutes % 60 !== 0) && (seconds % 60 === 0)) {
+    time = `${hours}h ${minutes}m`;
+  } else {
+    time = `${hours}h ${seconds}s`;
   }
 
   return `${time}`;
