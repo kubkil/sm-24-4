3996ce23bd0774dbecf355d1b42807205dab2fda
Jakub Kilar <kuba.kilar@gmail.com>
2018-06-23 19:53:33 +0200

    add formatDate body

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 const formatDate = timeInSeconds => {
-  return `${timeInSeconds}s`;
+  const hours = Math.floor(timeInSeconds / 3600);
+  const minutes = Math.floor(timeInSeconds % 3600 / 60);
+  const seconds = Math.floor(timeInSeconds % 3600 % 60);
+
+  let time;
+  let showHours;
+  let showMinutes;
+
+  if(hours > 0) {
+    showHours = `${hours}h`;
+  }
+
+  if (minutes > 0) {
+    showMinutes = `${minutes}h`;
+  }
+
+  if (hours < 1 && minutes < 1) {
+    time = `${seconds}s`;
+  }
+
+  return `${time}`;
 }
 
 module.exports = formatDate;
